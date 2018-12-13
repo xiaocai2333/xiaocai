a0039eaa8fe4548c8f55fcd8395a77c8b7c6a2af
tracer-ics2018 <tracer@njuics.org>
2018-12-13 10:19:51 +0800

    >  compile U201514734 xiaocai Linux xiaocai-X455LJ 4.18.0-10-generic #11-Ubuntu SMP Thu Oct 11 15:13:55 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:19:51 up 52 min,  1 user,  load average: 0.12, 0.24, 0.34 c86980c49895aaa7b49042cb199024d1d349e13

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -20,7 +20,6 @@ typedef struct {
     uint32_t _32;
     uint16_t _16;
     uint8_t _8[2];
-    };
   } gpr[8];
 
   /* Do NOT change the order of the GPRs' definitions. */
@@ -31,7 +30,7 @@ typedef struct {
   struct{
   rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
   };
-
+};
   vaddr_t eip;
 
 } CPU_state;
