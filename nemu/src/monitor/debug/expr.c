67be7f11522fcd3e4035b755101a2ced9f770ea3
tracer-ics2018 <tracer@njuics.org>
2018-12-20 12:49:38 +0800

    >  compile U201514734 xiaocai Linux xiaocai-X455LJ 4.18.0-10-generic #11-Ubuntu SMP Thu Oct 11 15:13:55 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  12:49:38 up  4:54,  1 user,  load average: 0.67, 0.51, 0.46 9476cff443ed6dab958fe32c9901f9cd9fc316

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -231,8 +231,10 @@ int OP_CET(int p, int q){
   int oper[nr_token];
   x++;
   printf("%d\n", x);
-  if(x == 3)
+  if(x == 3){
+    
     return 0;
+  }
   for(int i = p; i <= q; i++){
     if(tokens[i].type >= 100 && tokens[i].type <= 105){
       if(IsOPerINBRA(i)){
