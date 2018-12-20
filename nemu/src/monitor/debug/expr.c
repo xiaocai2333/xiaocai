0619009e911d46be0c755937fd976994f586a021
tracer-ics2018 <tracer@njuics.org>
2018-12-20 09:59:09 +0800

    >  compile U201514734 xiaocai Linux xiaocai-X455LJ 4.18.0-10-generic #11-Ubuntu SMP Thu Oct 11 15:13:55 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  09:59:09 up  2:04,  1 user,  load average: 0.66, 0.58, 0.51 392d9bf28592c716feb7467c37216c397bb64f48

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -134,10 +134,7 @@ uint32_t expr(char *e, bool *success) {
   }
 
   else{
-
-    if(check_parentheses(0,nr_token - 1)){
-      printf("%d\n",eval(0,nr_token - 1));
-    }
+    printf("%d\n",eval(0,nr_token - 1));
     for(int i = 0; i < nr_token; i++){
       printf("%d     %s\n", tokens[i].type,tokens[i].str);
     }
