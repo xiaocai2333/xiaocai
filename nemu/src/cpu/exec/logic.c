1764d7388b99c786c752c4d1e8d0eb36cdd25557
tracer-ics2018 <tracer@njuics.org>
2018-12-28 11:27:42 +0800

    >  compile U201514734 xiaocai Linux xiaocai-X455LJ 4.18.0-13-generic #14-Ubuntu SMP Wed Dec 5 09:04:24 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:27:42 up  3:27,  1 user,  load average: 0.84, 0.63, 0.64 5a3568281c4d52f2fbceedeacc3d574e8d4a904c

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -14,7 +14,14 @@ make_EHelper(and) {
 }
 
 make_EHelper(xor) {
-  TODO();
+  //TODO();
+  rtl_xor(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+
+  rtl_set_CF(&t0);
+  rtl_set_OF(&t0);
+
+  
 
   print_asm_template2(xor);
 }
