acf8f723a07afba2259b616c1af00a3885bdc2cc
tracer-ics2018 <tracer@njuics.org>
2019-01-01 20:05:42 +0800

    >  compile U201514734 xiaocai Linux xiaocai-X455LJ 4.18.0-13-generic #14-Ubuntu SMP Wed Dec 5 09:04:24 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:05:42 up 54 min,  1 user,  load average: 3.14, 3.78, 2.70 ccaa3651decd6e61bff981f915c98411f790027a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,5 @@
 #include "nemu.h"
-#include "device/mmio.h"
+
 #define PMEM_SIZE (128 * 1024 * 1024)
 
 #define pmem_rw(addr, type) *(type *)({\
@@ -12,14 +12,11 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  int mmio_map = is_mmio(addr);       // mmip map num
-  return mmio_map != -1 ? mmio_read(addr, len, mmio_map) :
-         pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));}
+  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+}
 
 void paddr_write(paddr_t addr, uint32_t data, int len) {
-  int mmio_map = is_mmio(addr);
-  return mmio_map != -1 ? mmio_write(addr, len, data, mmio_map)
-                        : memcpy(guest_to_host(addr), &data, len);
+  memcpy(guest_to_host(addr), &data, len);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
