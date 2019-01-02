f5d70b787aee5f51ed738ad1ad6f4c9f6205a028
tracer-ics2018 <tracer@njuics.org>
2019-01-02 09:48:04 +0800

    >  compile U201514734 xiaocai Linux xiaocai-X455LJ 4.18.0-13-generic #14-Ubuntu SMP Wed Dec 5 09:04:24 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  09:48:04 up  1:12,  1 user,  load average: 0.81, 0.69, 0.59 cfade36fa511784e6f6f4841153dedc745322248

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -11,6 +11,7 @@ make_EHelper(jmp) {
 make_EHelper(jcc) {
   // the target address is calculated at the decode stage
   uint32_t cc = decoding.opcode & 0xf;
+  printf("decoding = %d\n", decoding.opcode);
   printf("jcc = %d\n", cc);
   rtl_setcc(&t0, cc);
   rtl_li(&t1, 0);
