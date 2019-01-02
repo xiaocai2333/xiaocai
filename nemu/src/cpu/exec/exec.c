054d6a559fe8d4e3eccb4edbee4ba5ce6c83efb5
tracer-ics2018 <tracer@njuics.org>
2019-01-02 11:36:42 +0800

    >  compile U201514734 xiaocai Linux xiaocai-X455LJ 4.18.0-13-generic #14-Ubuntu SMP Wed Dec 5 09:04:24 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:36:42 up  3:00,  1 user,  load average: 0.38, 0.57, 0.63 6a619b7eca46d42aa6cb2427893e15fbf897dbaf

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -46,13 +46,13 @@ make_group(gp1,
 
   /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
 make_group(gp2,
-    EMPTY, EMPTY, EMPTY, EMPTY,
-    EMPTY, EMPTY, EMPTY, EMPTY)
+    EX(rol), EMPTY, EMPTY, EMPTY,
+    EX(shl), EX(shr), EMPTY, EX(sar))
 
   /* 0xf6, 0xf7 */
 make_group(gp3,
-    EMPTY, EMPTY, EMPTY, EMPTY,
-    EMPTY, EMPTY, EMPTY, EMPTY)
+    IDEX(test_I, test), EMPTY, EX(not), EX(neg),
+    EX(mul), EX(imul1), EX(div), EX(idiv))
 
   /* 0xfe */
 make_group(gp4,
@@ -100,7 +100,7 @@ opcode_entry opcode_table [512] = {
   /* 0x5c */  IDEX(r, pop), IDEX(r, pop), IDEX(r, pop), IDEX(r, pop),
   /* 0x60 */  EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x64 */  EMPTY, EMPTY, EX(operand_size), EMPTY,
-  /* 0x68 */  EMPTY, EMPTY, IDEX(r, push), EMPTY,
+  /* 0x68 */  IDEX(push_SI, push), IDEX(I_E2G, imul2), IDEXW(push_SI, push, 1), IDEX(SI_E2G, imul2),
   /* 0x6c */  EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x70 */  IDEXW(J, jcc, 1), IDEXW(J, jcc, 1), IDEXW(J, jcc, 1), IDEXW(J, jcc, 1),
   /* 0x74 */  IDEXW(J, jcc, 1), IDEXW(J, jcc, 1), IDEXW(J, jcc, 1), IDEXW(J, jcc, 1),
@@ -117,7 +117,7 @@ opcode_entry opcode_table [512] = {
   /* 0xa0 */  IDEXW(O2a, mov, 1), IDEX(O2a, mov), IDEXW(a2O, mov, 1), IDEX(a2O, mov),
   /* 0xa4 */  EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xa8 */  IDEXW(I2a, test, 1), IDEX(I2a, test), EMPTY, EMPTY,
-  /* 0xac */  EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0xac */  EMPTY, EMPTY, EMPTY, IDEX(E2G, imul2),
   /* 0xb0 */  IDEXW(mov_I2r, mov, 1), IDEXW(mov_I2r, mov, 1), IDEXW(mov_I2r, mov, 1), IDEXW(mov_I2r, mov, 1),
   /* 0xb4 */  IDEXW(mov_I2r, mov, 1), IDEXW(mov_I2r, mov, 1), IDEXW(mov_I2r, mov, 1), IDEXW(mov_I2r, mov, 1),
   /* 0xb8 */  IDEX(mov_I2r, mov), IDEX(mov_I2r, mov), IDEX(mov_I2r, mov), IDEX(mov_I2r, mov),
@@ -184,11 +184,11 @@ opcode_entry opcode_table [512] = {
   /* 0xa0 */  EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xa4 */  EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xa8 */  EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xac */  EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0xac */  EMPTY, EMPTY, EMPTY, IDEX(E2G, imul2),
   /* 0xb0 */  EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xb4 */  EMPTY, EMPTY, IDEXW(mov_E2G, movzx, 1), IDEXW(mov_E2G, movzx, 2),
   /* 0xb8 */  EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xbc */  EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0xbc */  EMPTY, EMPTY, IDEXW(mov_E2G, movzx, 1), IDEXW(mov_E2G, movzx, 2),
   /* 0xc0 */  EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xc4 */  EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xc8 */  EMPTY, EMPTY, EMPTY, EMPTY,
