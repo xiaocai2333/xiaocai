a3e5be69e98efdae90bfcac643c65c5e5098774b
tracer-ics2018 <tracer@njuics.org>
2019-01-03 20:30:51 +0800

    >  compile U201514734 xiaocai Linux xiaocai-X455LJ 4.18.0-13-generic #14-Ubuntu SMP Wed Dec 5 09:04:24 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:30:51 up  1:54,  1 user,  load average: 0.45, 0.50, 0.50 4709e26c287b08d399e99dec95a26dc7537b9923

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,8 +6,18 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+  //TODO();
+	  rtl_push(&cpu.flags);
+	  rtl_push(&cpu.cs);
+	  rtl_push(&ret_addr);
+	  cpu.eflags.IF = 0;
+	  uint32_t idtr_addr = cpu.idtr.base + (NO << 3);
+		uint32_t offset_low, offset_high;
+		offset_low = vaddr_read(idtr_addr, 4) & 0xffff;
+		offset_high = vaddr_read(idtr_addr + 4, 4) & 0xffff0000;
+	  rtl_j(offset_high | offset_low);
 }
 
 void dev_raise_intr() {
+	cpu.INTR = true;
 }
