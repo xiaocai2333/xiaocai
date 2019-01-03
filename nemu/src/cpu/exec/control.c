3ba18dedb2a12b6a88346c373fc925911ebcbdb3
tracer-ics2018 <tracer@njuics.org>
2019-01-03 12:36:25 +0800

    >  compile U201514734 xiaocai Linux xiaocai-X455LJ 4.18.0-13-generic #14-Ubuntu SMP Wed Dec 5 09:04:24 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  12:36:25 up  4:31,  1 user,  load average: 0.52, 0.66, 0.63 fd8dfff53da08fca69c3712f5c27bfd827477204

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -26,8 +26,9 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  TODO();
-
+  //TODO();
+  rtl_push(eip);
+  rtl_j(decoding.jmp_eip);
   print_asm("call %x", decoding.jmp_eip);
 }
 
