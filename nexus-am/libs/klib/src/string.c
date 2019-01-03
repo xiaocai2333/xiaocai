682f1117cf0cf87a25e12f1fe1392a505f62497b
tracer-ics2018 <tracer@njuics.org>
2019-01-03 21:24:26 +0800

    >  run U201514734 xiaocai Linux xiaocai-X455LJ 4.18.0-13-generic #14-Ubuntu SMP Wed Dec 5 09:04:24 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:24:26 up  2:48,  1 user,  load average: 0.71, 0.66, 0.56 187adb1b18c6e15f27a2bcda9256fe58e537d35c

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,36 +3,19 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  size_t len;
-  for (len = 0; s[len] != '\0'; len++);
-  return len;
+  return 0;
 }
 
 char *strcpy(char* dst,const char* src) {
-  assert(dst != NULL && src != NULL);
-  char* r = dst;
-  while ((*r++ = *src++) != '\0');
-  return dst;
+  return NULL;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  assert(dst != NULL && src != NULL);
-  size_t i;
-  for (i = 0; i < n && src[i] != '\0'; i++) {
-    dst[i] = src[i];
-  }
-  while (i < n) {
-    dst[i++] = '\0';
-  }
-  return dst;
+  return NULL;
 }
 
 char* strcat(char* dst, const char* src) {
-  assert(dst != NULL && src != NULL);
-  char* s;
-  for (s = dst; *s != '\0'; s++);
-  while ((*s++ = *src++) != '\0');
-  return dst;
+  return NULL;
 }
 
 int strcmp(const char* s1, const char* s2) {
@@ -49,16 +32,7 @@ int strcmp(const char* s1, const char* s2) {
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  assert(s1 != NULL && s2 != NULL);
-  size_t i;
-  n--;
-  for (i = 0; s1[i] != '\0' && s1[i] == s2[i] && i < n; i++);
-  if (s1[i] > s2[i]) {
-    return 1;
-  }
-  if (s1[i] < s2[i]) {
-    return -1;
-  }
+  
   return 0;
 }
 
@@ -71,21 +45,7 @@ void* memset(void* v,int c,size_t n) {
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  unsigned long long* dst = (unsigned long long*)out;
-  unsigned long long* src = (unsigned long long*)in;
-  size_t num_8 = n >> 3;
-  int i;
-  for (i = 0; i < num_8; i++) {
-    dst[i] = src[i];
-  }
-  num_8 <<= 3;
-  unsigned char* d = (unsigned char*)out + num_8;
-  unsigned char* s = (unsigned char*)in + num_8;
-  n -= num_8;
-  for (i = 0; i < n; i++) {
-    d[i] = s[i];
-  }
-  return out;
+  return NULL;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
