26bba91fe101558e983556e0e915530192b1f70b
tracer-ics2018 <tracer@njuics.org>
2019-01-04 10:46:42 +0800

    >  run U201514734 xiaocai Linux xiaocai-X455LJ 4.18.0-13-generic #14-Ubuntu SMP Wed Dec 5 09:04:24 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:46:42 up  2:37,  1 user,  load average: 0.99, 0.76, 0.82 984f00e849765b25af2060065960bc54f96965ca

--- nexus-am/am/arch/x86-nemu/src/devices/timer.c
+++ nexus-am/am/arch/x86-nemu/src/devices/timer.c
@@ -9,8 +9,7 @@ size_t timer_read(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_TIMER_UPTIME: {
       _UptimeReg *uptime = (_UptimeReg *)buf;
       uptime->hi = 0;
-      // uptime->lo = inl(0x48) - com_time;
-      uptime->lo = 0;
+      uptime->lo = inl(0x48) - com_time;
       return sizeof(_UptimeReg);
     }
     case _DEVREG_TIMER_DATE: {
