980a3528a9cfdb02fa6b374b026a72fe30bfc5f7
tracer-ics2018 <tracer@njuics.org>
2019-01-05 16:58:21 +0800

    >  run U201514734 xiaocai Linux xiaocai-X455LJ 4.18.0-13-generic #14-Ubuntu SMP Wed Dec 5 09:04:24 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:58:21 up  1:25,  1 user,  load average: 0.95, 0.78, 0.75 59eba1d4b9f960d1b400386b7af31a6c5987bb4f

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -6,8 +6,9 @@ size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
-      kbd->keydown = inl(0x60);
-      kbd->keycode = kbd->keycode & 0x8000;
+      uint32_t data = inl(0x60);
+      kbd->keydown = data & 0x8000 ? 1:0;
+      kbd->keycode = data & ~0x8000;
       return sizeof(_KbdReg);
     }
   }
